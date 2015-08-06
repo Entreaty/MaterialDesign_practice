40b77992c0ea8425d86f62aad3063c4f0a3362b9
Entreaty <ItmSpace@gmail.com>
2015-08-06 18:50:56 +0600

    lazy

---
+++ Card.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1" charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="./MDL/material.min.css">
+    <script src="./MDL/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!--    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.red-orange.min.css" />-->
+    <link rel="stylesheet"
+          href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.blue_grey-indigo.min.css"/>
+    <link href="http://allfont.ru/allfont.css?fonts=roboto-condensed-regular" rel="stylesheet" type="text/css"/>
+    <!--AngularJS Filter-->
+    <!--    Popup window-->
+    <!--    Parallax-->
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="Parallax/js/index.js"></script>
+    <style>
+        .background {
+            background: url("http://get.whotrades.com/u3/photo972D/20161575033-0/original.jpeg") repeat;
+            background-size: 140% auto;
+            position: fixed;
+            width: 100%;
+            height: 300%;
+            top: 0;
+            left: 0;
+
+        }
+
+        .wrapper {
+            padding: 75px 28px;
+            min-height: 1200px;
+            height: auto;
+            width: 75%;
+            margin: auto;
+            background: #fff;
+            opacity: 0.9;
+        }
+    </style>
+<body ng-app>
+<!-- Simple header with scrollable tabs. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <!--Верхнее меню -->
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <!-- Поиск в обычном режиме -->
+            <form action="#" method="get"><div style="margin-top: 15pt" class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--large-screen-only"><input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label class="mdl-textfield__label" for="sample3" style="color:#f5f5f5">Введите название или категорию... </label><button style="visibility: hidden" type="submit" id="sample4"></button></div></form><label class="mdl-button mdl-js-button mdl-button--icon mdl-layout--large-screen-only" for="sample4"><i class="material-icons">search</i></label>
+            <!-- Navigation. We hide it in small screens. -->
+            <div class="mdl-navigation mdl-layout--large-screen-only">
+                <a href="#films" class="mdl-navigation__link">Фильмы</a>
+                <a href="#serials" class="mdl-navigation__link">Сериалы</a>
+                <a href="#music" class="mdl-navigation__link">Музыка</a>
+                <a href="#cartoons" class="mdl-navigation__link">Мультфильмы</a>
+                <a href="#anime" class="mdl-navigation__link">Аниме</a>
+                <a href="#games" class="mdl-navigation__link">Игры</a>
+            </div>
+        </div>
+
+    </header>
+    <!--/Верхнее меню -->
+    <!-- LeftMenu -->
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Title</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here --></div>
+    </main>
+</div>
+
+<?php
+$request = "http://78.140.13.90:8080/api/feed";
+$json = file_get_contents($request);
+$obj = json_decode($json, true);      // Получили массив с фильмами
+?>
+<div class="background">
+    <a style="position: absolute;width: 100%;height: 100%;cursor: pointer"
+       href="index.php">
+    </a>
+    <!--<img src="http://get.whotrades.com/u3/photo972D/20161575033-0/original.jpeg">-->
+</div>
+
+<div class="wrapper">
+    <?php
+    $title = $_GET['title'];
+    foreach ($obj as $ff) {
+        if(array_search($title, $ff, true)) {
+            global $id,$title,$country,$genre,$year,$producer,
+                   $actors,$length,$kinopoiskId,$subject,$posterUrl,$quality;
+             $id = $ff['id'];
+             $title = $ff['title'];
+             $country = $ff['country'];
+             $genre = $ff['genre'];
+             $year = $ff['year'];
+             $producer = $ff['producer'];
+             $actors = $ff['actors'];
+             $length = $ff['length'];
+             $kinopoiskId = $ff['kinopoiskId'];
+             $subject = $ff['subject'];
+             $posterUrl = $ff['posterUrl'];
+             $quality = $ff['quality'];
+        }
+    }
+    ?>
+    <p class="pageSubTitle">Фильмы, <?=$year?></p>
+
+    <p class="pageTitle"><?=$title?></p>
+
+    <p class="pageSubTitle"><?=$genre?></p>
+    </header>
+    <section>
+        <div class="demo-grid-ruler mdl-grid">
+            <img style="height: auto;width:auto; max-height:700px;max-width:400px;margin-left: -8px"
+                 class="mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone"
+                 src="<?=$posterUrl?>">
+<!--                 src="http://24.media.tumblr.com/5fcd2cd475bfd67fc12de7ef4c473d23/tumblr_mrvx29VT5u1s01hr2o1_1280.jpg">-->
+
+            <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-cell--4-col-phone" style="">
+                <p>
+                    <i>
+                        <?if($title){echo 'Оригинальное название: ';}?>
+                    </i>
+                    <i>
+                        <?if($title)echo$title?>
+                    </i>
+                </p>
+
+                <p>
+                    <i>
+                        <?if($country){echo 'Страна: ';}?>
+                    </i>
+                    <i>
+                        <?if($country)echo$country?>
+                    </i>
+                </p>
+
+                <p>
+                    <i>
+                        <?if($producer){echo 'Режиссер: ';}?>
+                    </i>
+                    <i>
+                        <?if($producer)echo$producer?>
+                    </i>
+                </p>
+
+                <p>
+                    <i>
+                        <?if($actors){echo 'Актеры: ';}?>
+                    </i>
+                    <i>
+                        <?if($actors)echo$actors?>
+                    </i>
+                </p>
+
+                <p>
+                    <i>
+                        <?if($length){echo 'Время: ';}?>
+                    </i>
+                    <i>
+                        <?if($length)echo$length?>
+                    </i>
+                </p>
+
+                <p>
+                    <i>
+                        <?if($quality){echo 'Качество: ';}?>
+                    </i>
+                    <i>
+                        <?if($quality)echo$quality?>
+                    </i>
+                </p>
+            </div>
+
+        </div>
+
+        <div>
+            <p>
+                <i>
+                    <?=$subject?>
+                </i>
+            </p>
+        </div>
+
+    </section>
+</div>
+
+
+</body>
--- index.php
+++ index.php
@@ -10,23 +10,23 @@
     <!--    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.red-orange.min.css" />-->
     <link rel="stylesheet"
           href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.blue_grey-indigo.min.css"/>
-    <link href="http://allfont.ru/allfont.css?fonts=roboto-condensed-regular" rel="stylesheet" type="text/css"/>
+<!--    <link href="http://allfont.ru/allfont.css?fonts=roboto-condensed-regular" rel="stylesheet" type="text/css"/>-->
+    <!--    JQuery-->
+    <script src="./jquery-2.1.4.js"></script>
+    <!--    <script src="./lazy/js/jquery-2.0.3.min.js"></script>-->
     <!--AngularJS Filter-->
     <link rel="stylesheet" href="./newFilter/css/normalize.css">
     <script src="./newFilter/js/prefixfree.min.js"></script>
-    <script src='./AngularJS/angular.min.js'></script>
     <script src="./newFilter/js/index.js"></script>
     <script src="./newFilter/js/index.js"></script>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.min.js'></script>
     <!--    Popup window-->
+
+    <!--    Bootstrap-->
+<!--    <script src='/bootstrap/ui-bootstrap-tpls-0.13.2.min.js'></script>-->
     <!--    Parallax-->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="Parallax/js/index.js"></script>
-    <!--    <style>-->
-    <!--        .waterfall-demo-header-nav .mdl-navigation__link:last-of-type {-->
-    <!--            padding-right: 0;-->
-    <!--        }-->
-    <!--    </style>-->
     <style>
         a {
             color: dimgray;
@@ -81,136 +81,123 @@
 <body ng-app>
 <!-- Simple header with scrollable tabs. -->
 <div class=" mdl-layout mdl-js-layout mdl-layout--fixed-header ">
-<!--Верхнее меню -->
-<header class=" mdl-layout__header mdl-layout__header--waterfall">
-    <div class="mdl-layout__header-row">
+    <!--Верхнее меню -->
+    <header class=" mdl-layout__header mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+            <!-- Поиск в обычном режиме -->
+            <form action="#" method="get">
+                <div style="margin-top: 15pt"
+                     class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--large-screen-only">
+                    <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label
+                        class="mdl-textfield__label" for="sample3" style="color:#f5f5f5">Введите название или
+                        категорию... </label>
+                    <button style="visibility: hidden" type="submit" id="sample4"></button>
+                </div>
+            </form>
+            <label class="mdl-button mdl-js-button mdl-button--icon mdl-layout--large-screen-only" for="sample4"><i
+                    class="material-icons">search</i></label>
+            <!-- Spaceer -->
+            <div class="mdl-layout-spacer"></div>
+            <!-- Filter -->
+            <div
+                class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                <label class="mdl-button mdl-js-button mdl-button--icon" for="waterfall-exp"><i id="tt2"
+                                                                                                class="material-icons">filter</i></label>
+
+                <div class="mdl-textfield__expandable-holder"><input ng-model="search" class="mdl-textfield__input"
+                                                                     type="text" name="sample" id="waterfall-exp"/>
+                </div>
+                <div style="background-color: #ffffff; color:dimgray" class="mdl-tooltip">Фильтр</div>
+            </div>
+        </div>
         <!--Navigation-->
-        <nav  class="mdl-navigation mdl-cell--hide-tablet mdl-cell--hide-phone">
-            <a class="mdl-navigation__link" href="">Фильмы</a>
-            <a class="mdl-navigation__link" href="">Сериалы</a>
-            <a class="mdl-navigation__link" href="">Музыка</a>
-            <a class="mdl-navigation__link" href="">Мультфильмы</a>
-            <a class="mdl-navigation__link" href="">Аниме</a>
-            <a class="mdl-navigation__link" href="">Игры</a>
-        </nav>
-        <!-- Title -->
-        <!-- Поиск в режиме телефона -->
+        <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+            <a href="#films" class="mdl-layout__tab is-active">Фильмы</a>
+            <a href="#serials" class="mdl-layout__tab">Сериалы</a>
+            <a href="#music" class="mdl-layout__tab">Музыка</a>
+            <a href="#cartoons" class="mdl-layout__tab">Мультфильмы</a>
+            <a href="#anime" class="mdl-layout__tab">Аниме</a>
+            <a href="#games" class="mdl-layout__tab">Игры</a>
+        </div>
+    </header>
+    <!--/Верхнее меню -->
+    <!-- LeftMenu -->
+    <div class="mdl-layout__drawer">
         <form action="#" method="get">
             <div
-                class="mdl-textfield mdl-js-textfield mdl-textfield--expandable textfield-demo mdl-layout--small-screen-only">
-                <label class="mdl-button mdl-js-button mdl-button--icon" for="sample6">
-                    <i class="material-icons">search</i>
-                </label>
-
-                <div class="mdl-textfield__expandable-holder">
-                    <input class="mdl-textfield__input" type="text" id="sample6"/>
-                    <label class="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--small-screen-only">
+                <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label
+                    class="mdl-textfield__label" for="sample3" style="color:dimgray">Поиск...</label></div>
+        </form>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="#films">Фильмы</a>
+            <a class="mdl-navigation__link" href="#serials">Сериалы</a>
+            <a class="mdl-navigation__link" href="#music">Музыка</a>
+            <a class="mdl-navigation__link" href="#cartoons">Мультфильмы</a>
+            <a class="mdl-navigation__link" href="#anime">Аниме</a>
+            <a class="mdl-navigation__link" href="#games">Игры</a>
+        </nav>
+    </div>
+    <!-- /LeftMenu -->
+    <!-- mainContent-->
+    <main id="output" class="mdl-layout__content" ng-controller="myCtrl">
+        <section class="mdl-layout__tab-panel is-active " id="films">
+            <!-- Your content goes here -->
+            <div class="page-content">
+                <div id="output2" class="demo-grid-ruller mdl-grid">
+                    <div ng-animate="'animate'"
+                         ng-repeat="result in data | filter:search"
+                         align="center"
+                         class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
+                        <a href="{{}}">
+<!--                            <img src="{{}}">-->
+                            <a style="text-decoration: none;" href="Card.php?title={{result.title}}">
+                                <h5>
+                                    {{result.title}}
+                                </h5>
+                            </a>
+                        </a>
+                    </div>
+                </div>
+                <button style="top: 20%; position: fixed" onclick="myFunction()">Click me to scroll</button>
+                <script>
+                </script>
+            </div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="serials">
+            <div class="page-content"><!-- Your content goes here -->
+                <div class="page-content">
+                    <div class="demo-grid-ruller mdl-grid">
+                        <div ng-animate="'animate'"
+                             ng-repeat="result in data | filter:search"
+                             align="center"
+                             class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
+                            <a href="{{}}">
+<!--                                <img src="{{}}">-->
+                                <a style="text-decoration: none;" href="Card.php?id={{result.title}}">
+                                    <h5>
+                                        {{result.title}} 222222
+                                    </h5>
+                                </a>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </form>
-        <!-- Spaceer -->
-        <div class="mdl-layout-spacer"></div>
-        <!-- Поиск в обычном режиме -->
-        <form action="#" method="get">
-            <div style="margin-top: 15pt"
-                 class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--large-screen-only">
-                <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/>
-                <label class="mdl-textfield__label" for="sample3" style="color:#f5f5f5">Введите название или
-                    категорию... </label>
-                <button style="visibility: hidden" type="submit" id="sample4"></button>
-            </div>
-        </form>
-        <label class="mdl-button mdl-js-button mdl-button--icon mdl-layout--large-screen-only" for="sample4">
-            <i class="material-icons">search</i>
-        </label>
-
-        <!-- Filter -->
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right">
-            <label class="mdl-button mdl-js-button mdl-button--icon"
-                   for="waterfall-exp">
-                <i id="tt2" class="material-icons">filter</i>
-            </label>
-
-            <div class="mdl-textfield__expandable-holder">
-                <input ng-model="search" class="mdl-textfield__input" type="text" name="sample" id="waterfall-exp"/>
-            </div>
-            <div style="background-color: #ffffff; color:dimgray" class="mdl-tooltip">
-                Фильтр
-            </div>
-        </div>
-        <!-- /Filter -->
-    </div>
-    <!-- Tabs -->
-    <!--        <div class="mdl-layout__tab-bar mdl-js-ripple-effect" style="height: 48px">-->
-    <!--            <a href="#scroll-tab-films" class="mdl-layout__tab is-active">Фильмы</a>-->
-    <!--            <a href="#scroll-tab-serials" class="mdl-layout__tab">Сериалы</a>-->
-    <!--            <a href="#scroll-tab-music" class="mdl-layout__tab">Музыка</a>-->
-    <!--            <a href="#scroll-tab-cartoons" class="mdl-layout__tab">Мультфильмы</a>-->
-    <!--            <a href="#scroll-tab-anime" class="mdl-layout__tab">Аниме</a>-->
-    <!--            <a href="#scroll-tab-games" class="mdl-layout__tab">Игры</a>-->
-    <!--        </div>-->
-</header>
-<!--/Верхнее меню -->
-<!-- LeftMenu -->
-<div class="mdl-layout__drawer">
-    <span class="mdl-layout-title">Чебурда</span>
-
-    <form action="#" method="get">
-        <div
-            class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--small-screen-only">
-            <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/>
-            <label class="mdl-textfield__label" for="sample3" style="color:dimgray">Поиск...</label>
-        </div>
-    </form>
-    <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Фильмы</a>
-        <a class="mdl-navigation__link" href="">Сериалы</a>
-        <a class="mdl-navigation__link" href="">Музыка</a>
-        <a class="mdl-navigation__link" href="">Мультфильмы</a>
-        <a class="mdl-navigation__link" href="">Аниме</a>
-        <a class="mdl-navigation__link" href="">Игры</a>
-    </nav>
-</div>
-<!-- /LeftMenu -->
-<!-- mainContent-->
-<main class=" mdl-layout__content" ng-controller="myCtrl">
-<section class="mdl-layout__tab-panel is-active " id="scroll-tab-films">
-    <!-- Your content goes here -->
-    <div class="page-content">
-        <div class="demo-grid-ruller mdl-grid">
-            <div ng-animate="'animate'"
-                 ng-repeat="result in data | filter:search"
-                 align="center"
-                 class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
-                <a href="{{result.picture_big}}">
-                    <img src="{{result.picture}}">
-                    <a style="text-decoration: none;" href="#popup1">
-                        <h5>
-                            {{result.title}}
-                        </h5>
-                    </a>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
-<section class="mdl-layout__tab-panel" id="scroll-tab-serials">
-    <div class="page-content"><!-- Your content goes here --></div>
-</section>
-<section class="mdl-layout__tab-panel" id="scroll-tab-music">
-    <div class="page-content"><!-- Your content goes here --></div>
-</section>
-<section class="mdl-layout__tab-panel" id="scroll-tab-cartoons">
-    <div class="page-content"><!-- Your content goes here --></div>
-</section>
-<section class="mdl-layout__tab-panel" id="scroll-tab-anime">
-    <div class="page-content"><!-- Your content goes here --></div>
-</section>
-<section class="mdl-layout__tab-panel" id="scroll-tab-games">
-    <div class="page-content"><!-- Your content goes here --></div>
-</section>
-</main>
+        </section>
+        <section class="mdl-layout__tab-panel" id="music">
+            <div class="page-content"><!-- Your content goes here --></div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="cartoons">
+            <div class="page-content"><!-- Your content goes here --></div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="anime">
+            <div class="page-content"><!-- Your content goes here --></div>
+        </section>
+        <section class="mdl-layout__tab-panel" id="games">
+            <div class="page-content"><!-- Your content goes here --></div>
+        </section>
+    </main>
 </div>
 
 
