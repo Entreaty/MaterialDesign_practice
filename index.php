c31f3e59f343c118ca5ddc1b842a37bec08d7ae1
Entreaty <ItmSpace@gmail.com>
2015-08-18 17:29:50 +0600

    Last angular vers

--- index.php
+++ index.php
@@ -4,200 +4,215 @@
     <meta charset="UTF-8">
     <meta name=viewport content="width=device-width, initial-scale=1.0" charset="UTF-8">
     <title></title>
+
     <link rel="stylesheet" href="./MDL/material.min.css">
     <script src="./MDL/material.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <!--    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.red-orange.min.css" />-->
-    <link rel="stylesheet"
-          href="https://storage.googleapis.com/code.getmdl.io/1.0.1/material.blue_grey-indigo.min.css"/>
-<!--    <link href="http://allfont.ru/allfont.css?fonts=roboto-condensed-regular" rel="stylesheet" type="text/css"/>-->
+
     <!--    JQuery-->
     <script src="./jquery-2.1.4.js"></script>
-    <!--    <script src="./lazy/js/jquery-2.0.3.min.js"></script>-->
+
     <!--AngularJS Filter-->
-    <link rel="stylesheet" href="./newFilter/css/normalize.css">
-    <script src="./newFilter/js/prefixfree.min.js"></script>
-    <script src="./newFilter/js/index.js"></script>
     <script src="./newFilter/js/index.js"></script>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.min.js'></script>
-    <!--    Popup window-->
-
-    <!--    Bootstrap-->
-<!--    <script src='/bootstrap/ui-bootstrap-tpls-0.13.2.min.js'></script>-->
     <!--    Parallax-->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="Parallax/js/index.js"></script>
     <style>
-        a {
-            color: dimgray;
-            text-decoration: none;
+        /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+        @import url(http://fonts.googleapis.com/css?family=Roboto:400,300);
+        .loading-bro {
+            top: 25%;
+            left: 45%;
+            /*padding: 25% 25%;*/
+            /*margin: 25% auto;*/
+            width: 150px;
+            position: fixed;
         }
-
-        A:hover {
-            /*text-decoration: underline; *//* Добавляем подчеркивание при наведении курсора на ссылку */
-            color: orangered;
-
-            /* Ссылка красного цвета */
-        }
-    </style>
-    <style>
-        .animate-enter, .animate-leave {
-            transition: 200ms ease-in all;
-            position: relative;
-            display: block;
-        }
-
-        .animate-enter.animate-enter-active, .animate-leave {
-            left: 0;
-        }
-
-        .animate-leave.animate-leave-active, .animate-enter {
-            left: 200px;
-        }
-    </style>
-    <style>
-        .background {
-            /*background: url("http://get.whotrades.com/u3/photo972D/20161575033-0/original.jpeg") repeat;*/
-            /*background-size: 140% auto;*/
-            /*position: fixed;*/
-            /*width: 100%;*/
-            /*height: 300%;*/
-            /*top: 0;*/
-            /*left: 0;*/
-            /*z-index: -1*/
-            background: rgba(0, 0, 0, 0.5);
-        }
-
-        .wrapper {
-            min-height: 1200px;
-            height: auto;
+        .loading-bro > h1 {
             width: 75%;
-            margin: auto;
-            background: #fff;
-            opacity: 0.9;
+            text-align: center;
+            font-size: 2.5em;
+            margin-bottom: 1em;
+            font-weight: 300;
+            color: #8E8E8E;
+        }
+
+        #load {
+            width: 75%;
+            animation: loading 3s linear infinite;
+        }
+        #load #loading-inner {
+            stroke-dashoffset: 0;
+            stroke-dasharray: 300;
+            stroke-width: 10;
+            stroke-miterlimit: 10;
+            stroke-linecap: round;
+            animation: loading-circle 2s linear infinite;
+            stroke: #51BBA7;
+            fill: transparent;
+        }
+
+        @keyframes loading {
+            0% {
+                transform: rotate(0);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+        @keyframes loading-circle {
+            0% {
+                stroke-dashoffset: 0;
+            }
+            100% {
+                stroke-dashoffset: -600;
+            }
+        }
+
+    </style>
+    <style>
+        .unvisible{
+            display: none;
         }
     </style>
-</head>
-<body ng-app>
-<!-- Simple header with scrollable tabs. -->
-<div class=" mdl-layout mdl-js-layout mdl-layout--fixed-header ">
-    <!--Верхнее меню -->
-    <header class=" mdl-layout__header mdl-layout__header--waterfall">
-        <div class="mdl-layout__header-row">
-            <!-- Поиск в обычном режиме -->
-            <form action="#" method="get">
-                <div style="margin-top: 15pt"
-                     class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--large-screen-only">
-                    <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label
-                        class="mdl-textfield__label" for="sample3" style="color:#f5f5f5">Введите название или
-                        категорию... </label>
-                    <button style="visibility: hidden" type="submit" id="sample4"></button>
-                </div>
-            </form>
-            <label class="mdl-button mdl-js-button mdl-button--icon mdl-layout--large-screen-only" for="sample4"><i
-                    class="material-icons">search</i></label>
-            <!-- Spaceer -->
-            <div class="mdl-layout-spacer"></div>
-            <!-- Filter -->
-            <div
-                class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-                <label class="mdl-button mdl-js-button mdl-button--icon" for="waterfall-exp"><i id="tt2"
-                                                                                                class="material-icons">filter</i></label>
 
-                <div class="mdl-textfield__expandable-holder"><input ng-model="search" class="mdl-textfield__input"
-                                                                     type="text" name="sample" id="waterfall-exp"/>
-                </div>
-                <div style="background-color: #ffffff; color:dimgray" class="mdl-tooltip">Фильтр</div>
-            </div>
-        </div>
-        <!--Navigation-->
-        <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-            <a href="#films" class="mdl-layout__tab is-active">Фильмы</a>
-            <a href="#serials" class="mdl-layout__tab">Сериалы</a>
-            <a href="#music" class="mdl-layout__tab">Музыка</a>
-            <a href="#cartoons" class="mdl-layout__tab">Мультфильмы</a>
-            <a href="#anime" class="mdl-layout__tab">Аниме</a>
-            <a href="#games" class="mdl-layout__tab">Игры</a>
-        </div>
-    </header>
-    <!--/Верхнее меню -->
-    <!-- LeftMenu -->
-    <div class="mdl-layout__drawer">
+</head>
+<body onload="loadPage()" ng-app>
+
+<div id="loading" class="loading-bro">
+    <h1>Loading</h1>
+    <svg id="load" x="0px" y="0px" viewBox="0 0 150 150">
+        <circle id="loading-inner" cx="75" cy="75" r="60"/>
+    </svg>
+</div>
+
+<!-- Simple header with scrollable tabs. -->
+<div     id="output" class="unvisible mdl-layout mdl-js-layout ">
+    <!--Верхнее меню -->
+    <div  class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+    <div
+        style="
+        position:fixed;
+    background-color: #0091ea;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    /*-webkit-box-orient: vertical;*/
+    /*-webkit-box-direction: normal;*/
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -webkit-flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    /*-webkit-flex-direction: column;*/
+    /*-ms-flex-direction: column;*/
+    /*flex-direction: column;*/
+    -webkit-flex-wrap: nowrap;
+    -ms-flex-wrap: nowrap;
+    flex-wrap: nowrap;
+    -webkit-box-pack: start;
+    -webkit-justify-content: flex-start;
+    -ms-flex-pack: start;
+    justify-content: flex-start;
+    box-sizing: border-box;
+    -webkit-flex-shrink: 0;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+    width: 90%;
+    margin-left: 5%;
+    padding: 15px;
+    border: none;
+    /*min-height: 50px;*/
+    max-height: 1000px;
+    height: 48px;
+    z-index: 3;
+    /*background-color: rgb(244, 67, 54);*/
+    color: rgb(255, 255, 255);
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 0 3px 1px -2px rgba(0, 0, 0, .2), 0 1px 5px 0 rgba(0, 0, 0, .12);
+    -webkit-transition-duration: .2s;
+    transition-duration: .2s;
+    -webkit-transition-timing-function: cubic-bezier(.4, 0, .2, 1);
+    transition-timing-function: cubic-bezier(.4, 0, .2, 1);
+    -webkit-transition-property: max-height, box-shadow;
+    transition-property: max-height, box-shadow;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-align-self: stretch;
+    -ms-flex-item-align: stretch;
+    align-self: stretch;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center"
+        >
+        <div class="mdl-layout-spacer"></div>
+        <!-- Поиск в обычном режиме -->
         <form action="#" method="get">
-            <div
-                class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo mdl-layout--small-screen-only">
-                <input class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label
-                    class="mdl-textfield__label" for="sample3" style="color:dimgray">Поиск...</label></div>
+            <div class="mdl-textfield mdl-js-textfield ">
+                <input ng-model="search" class="mdl-textfield__input" name="searchRequest" type="text" id="sample3"/><label
+                    class="mdl-textfield__label" for="sample3" >Введите название или
+                    категорию...
+                    <button style="visibility: hidden" type="submit" id="sample4"></button>
+                </label>
+            </div>
         </form>
-        <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="#films">Фильмы</a>
-            <a class="mdl-navigation__link" href="#serials">Сериалы</a>
-            <a class="mdl-navigation__link" href="#music">Музыка</a>
-            <a class="mdl-navigation__link" href="#cartoons">Мультфильмы</a>
-            <a class="mdl-navigation__link" href="#anime">Аниме</a>
-            <a class="mdl-navigation__link" href="#games">Игры</a>
-        </nav>
+
+        <!-- Spaceer -->
+        <div class="mdl-layout-spacer"></div>
+        <div  class="mdl-tabs__tab-bar">
+            <a href="#starks-panel" class="mdl-tabs__tab is-active">Фильмы</a>
+            <a href="#lannisters-panel" class="mdl-tabs__tab">Сериалы</a>
+            <a href="#targaryens-panel" class="mdl-tabs__tab">Игры</a>
+            <a href="#targaryens-panel" class="mdl-tabs__tab">Музыка</a>
+            <a href="#targaryens-panel" class="mdl-tabs__tab">Аниме</a>
+            <a href="#targaryens-panel" class="mdl-tabs__tab">Мультфильмы</a>
+        </div>
+        <div class="mdl-layout-spacer"></div>
     </div>
-    <!-- /LeftMenu -->
-    <!-- mainContent-->
-    <main id="output" class="mdl-layout__content" ng-controller="myCtrl">
-        <section class="mdl-layout__tab-panel is-active " id="films">
+
+
+
+
+
+        <div style="padding:100px 0;width:80%;margin-left:10%" class="mdl-tabs__panel is-active" id="starks-panel" ng-controller="myCtrl">
             <!-- Your content goes here -->
-            <div class="page-content">
-                <div id="output2" class="demo-grid-ruller mdl-grid">
-                    <div ng-animate="'animate'"
-                         ng-repeat="result in data | filter:search"
-                         align="center"
-                         class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
-                        <a href="{{}}">
-<!--                            <img src="{{}}">-->
-                            <a style="text-decoration: none;" href="Card.php?title={{result.title}}">
-                                <h5>
-                                    {{result.title}}
-                                </h5>
-                            </a>
-                        </a>
-                    </div>
-                </div>
-                <button style="top: 20%; position: fixed" onclick="myFunction()">Click me to scroll</button>
-                <script>
-                </script>
-            </div>
-        </section>
-        <section class="mdl-layout__tab-panel" id="serials">
-            <div class="page-content"><!-- Your content goes here -->
-                <div class="page-content">
-                    <div class="demo-grid-ruller mdl-grid">
-                        <div ng-animate="'animate'"
-                             ng-repeat="result in data | filter:search"
-                             align="center"
-                             class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
-                            <a href="{{}}">
-<!--                                <img src="{{}}">-->
-                                <a style="text-decoration: none;" href="Card.php?id={{result.title}}">
-                                    <h5>
-                                        {{result.title}} 222222
-                                    </h5>
-                                </a>
-                            </a>
-                        </div>
-                    </div>
+            <div id="output2" class="demo-grid-ruller mdl-grid">
+                <div ng-animate="'animate'"
+                     ng-repeat="result in data | filter:search"
+                     align="center"
+                     class="mdl-cell mdl-cell--2-col mdl-cell--4-col-tablet mdl-cell--2-col-phone">
+<!--                                                                        <img style="width: 150px;height: 200px" src="{{result.posterUrl}}">-->
+                    <a style="text-align: start; text-decoration: none;" href="Card.php?title={{result.title}}">
+                        <h5>
+                            {{result.title}}
+                        </h5>
+                        <h5></h5>
+                    </a>
                 </div>
             </div>
-        </section>
-        <section class="mdl-layout__tab-panel" id="music">
-            <div class="page-content"><!-- Your content goes here --></div>
-        </section>
-        <section class="mdl-layout__tab-panel" id="cartoons">
-            <div class="page-content"><!-- Your content goes here --></div>
-        </section>
-        <section class="mdl-layout__tab-panel" id="anime">
-            <div class="page-content"><!-- Your content goes here --></div>
-        </section>
-        <section class="mdl-layout__tab-panel" id="games">
-            <div class="page-content"><!-- Your content goes here --></div>
-        </section>
-    </main>
+
+        </div>
+        <div class="mdl-tabs__panel" id="lannisters-panel">
+            <ul>
+                <li>Tywin</li>
+                <li>Cersei</li>
+                <li>Jamie</li>
+                <li>Tyrion</li>
+            </ul>
+        </div>
+        <div class="mdl-tabs__panel" id="targaryens-panel">
+            <ul>
+                <li>Viserys</li>
+                <li>Daenerys</li>
+            </ul>
+        </div>
+    </div>
+    <!-- mainContent-->
+
 </div>
 
 
